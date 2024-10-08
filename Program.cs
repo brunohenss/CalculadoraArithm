741bbdc37a68195890c5a89a27059da04398fd47
Bruno Henrique dos Santos Silva <brunoricksp@gmail.com>
2024-10-07 22:26:43 -0300

    Add files via upload
    Projeto simples de estudo.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using OpeAritmMath;
+
+Calculadora calc = new Calculadora();
+
+calc.Somar(10, 15);
+
+calc.Dividir(100, 2);
+
+calc.Multiplicar(100, 100);
+
+calc.Subtrair(10000, 1);
+
+calc.Potencia(5, 10);
